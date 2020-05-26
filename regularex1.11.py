8a4ce2ad32931bac4c643cc1183dbfc2cc3efdc8
v-deepa <65391741+v-deepa@users.noreply.github.com>
2020-05-26 20:41:08 +0530

    sample commit

---
+++ regularex1.11.py
@@ -0,0 +1,46 @@
+import re
+fhandle=open("mbox1.txt")
+count=0
+numlist=list()
+for line in fhandle:
+
+    line=line.rstrip()
+
+    stuff=re.findall('^X-DSPAM-Confidence: ([0-9.]+)',line)
+
+    if (len(stuff) !=1):
+        continue
+    num=float(stuff[0])
+    numlist.append(num)
+
+    #if re.search('^X-\S+:', line):
+print("maximum",max(numlist))
+
+fhandle.close()
+
+
+# if re.search('^From.*:',line):
+# if re.search('^X-.*:', line):
+x="My favouite 2 number is 10 and 41"
+y=re.findall('[0-9]+',x)
+print(y)
+
+x="FRom: using a s: asdfsda"
+y=re.findall('^F.+:',x)
+print(y)
+y=re.findall('^F.+?:',x)
+print(y)
+
+Email="From cwen@iupui.edu Thu Jan  3 16:29:07 $2008"
+y=re.findall('^From (\S+@\S+)',Email)
+print(y)
+
+y=re.findall('^From .*@([^ ]*)',Email)
+print(y)
+
+y=re.findall(' [a-z0-9]',Email)
+print(y)
+
+f="From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008"
+y=re.findall('@(\S+)',f)
+print(y)
